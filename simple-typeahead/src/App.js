9474401f20982fcf4a5ba7f08c3b2fd3748f58cf
Sercan Leylek <leylek.sercan@gmail.com>
2020-05-23 14:10:25 +0200

    Create typeahead component

--- simple-typeahead/src/App.js
+++ simple-typeahead/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Typeahead from "./typeahead/typeahead";
 
 function App() {
   return (
     <div className="App">
-
+      <Typeahead/>
     </div>
   );
 }
---
+++ simple-typeahead/src/typeahead/typeahead.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Typeahead = () => {
+    return (
+        <div className="typeahead">
+
+        </div>
+    );
+};
+
+export default Typeahead;
