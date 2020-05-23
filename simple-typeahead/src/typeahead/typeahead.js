98795cc36179a9ff0f3828dd083689b3fd02d4dd
Sercan Leylek <leylek.sercan@gmail.com>
2020-05-23 14:24:47 +0200

    Add Jøkul's TextInput Component

--- simple-typeahead/src/typeahead/typeahead.js
+++ simple-typeahead/src/typeahead/typeahead.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { TextInput } from "@fremtind/jkl-text-input-react";
 
 const Typeahead = () => {
+    const [value, setValue] = useState('');
+    const [showSuggestions, setShowSuggestions] = useState(false);
+
     return (
         <div className="typeahead">
-
+            <TextInput
+                forceCompact={false}
+                action={{
+                    icon: 'clear',
+                    label: 'Clear the text',
+                    onClick: () => setValue(''),
+                }}
+                label=""
+                placeholder="Type to see some suggestions"
+                value={value}
+                width="100%"
+                onFocus={() => setShowSuggestions(true)}
+                onBlur={() => setShowSuggestions(false)}
+            />
         </div>
     );
 };
