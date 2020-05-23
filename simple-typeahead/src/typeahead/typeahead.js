1145b12df7079189a8e01942ac0c60e8ad61e6d0
Sercan Leylek <leylek.sercan@gmail.com>
2020-05-23 15:03:58 +0200

    Add Jøkul's useKeyListener Hook

--- simple-typeahead/src/typeahead/typeahead.js
+++ simple-typeahead/src/typeahead/typeahead.js
@@ -1,12 +1,35 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import { useKeyListener } from '@fremtind/jkl-react-hooks';
 
 const Typeahead = () => {
     const [value, setValue] = useState('');
     const [showSuggestions, setShowSuggestions] = useState(false);
 
+    const ref = useRef(null);
+    const keys = ['ArrowUp', 'ArrowDown', 'Enter', 'Escape', 'Backspace'];
+
+    const onKeyPressed = (event) => {
+        switch (event.key) {
+            case 'ArrowDown':
+                console.log("arrow down");
+                break;
+            default:
+                console.log("do nothing");
+                break;
+        }
+    };
+    useKeyListener(ref, keys, onKeyPressed);
+
     return (
         <div className="typeahead">
-
+            <input
+                ref={ref}
+                type="text"
+                value={value}
+                placeholder="Type to see some suggestions"
+                onFocus={() => setShowSuggestions(true)}
+                onBlur={() => setShowSuggestions(false)}
+            />
         </div>
     );
 };
